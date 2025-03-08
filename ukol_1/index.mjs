1e382b6a9750ba6832b564aaca2443b024662ce9
Michaela Hošková <66723714+misahoskova@users.noreply.github.com>
2025-03-08 20:09:51 +0100

    Ukol_1 finished

--- ukol_1/index.mjs
+++ ukol_1/index.mjs
@@ -4,7 +4,11 @@ const readFile = (name) => {
   return new Promise((resolve, reject) => {
     fs.readFile(name, 'utf8', (err, data) => {
       if (err) {
-        reject(err);
+        if (err.code === 'ENOENT') {
+          reject(new Error(`File not found: ${name}`));
+        } else {
+          reject(err);
+        }
       } else {
         resolve(data.toString());
       }
@@ -24,7 +28,23 @@ const writeFile = (name, data) => {
   });
 };
 
-readFile('instrukce.txt').then((instrukce) => {
-  const [input, output] = instrukce.split(' ');
-  console.log(input + ' - ' + output);
-});
+readFile('instrukce.txt')
+  .then((instrukce) => {
+    const [input, output] = instrukce.trim().split(' ');
+    console.log(`Reading: ${input}, writing to: ${output}`);
+
+    return readFile(input).then((inputText) => {
+      console.log('Input file:', inputText);
+      return writeFile(output, inputText);
+    });
+  })
+  .then(() => {
+    console.log('Output file has been saved!');
+  })
+  .catch((err) => {
+    if (err.message.startsWith('File not found')) {
+      console.error(err.message);
+    } else {
+      console.error('Error:', err);
+    }
+  });
