3c8c10769477c4886865f631ec9d93322bf82c83
Michaela Hošková <66723714+misahoskova@users.noreply.github.com>
2025-04-02 17:25:43 +0200

    ukol5

---
+++ ukol_5/index.js
@@ -0,0 +1,133 @@
+import { Hono } from "hono"
+import { serve } from "@hono/node-server"
+import { logger } from "hono/logger"
+import { serveStatic } from "@hono/node-server/serve-static"
+import { renderFile } from "ejs"
+import { drizzle } from "drizzle-orm/libsql"
+import { todosTable } from "./src/schema.js"
+import { eq } from "drizzle-orm"
+
+const db = drizzle({
+  connection: "file:db.sqlite",
+  logger: true,
+})
+
+const app = new Hono()
+
+app.use(logger())
+app.use(serveStatic({ root: "public" }))
+
+app.get("/", async (c) => {
+  const todos = await db.select().from(todosTable).all()
+
+  const index = await renderFile("views/index.html", {
+    title: "ÚKOLNÍČEK",
+    todos,
+  })
+
+  return c.html(index)
+})
+
+app.post("/todos", async (c) => {
+  const form = await c.req.formData()
+  const title = form.get("title")
+  const priority = form.get("priority") || "normal"
+
+  await db.insert(todosTable).values({
+    title,
+    done: false,
+    priority,
+  })
+
+  return c.redirect("/")
+})
+
+app.get("/todos/:id", async (c) => {
+  const id = Number(c.req.param("id"))
+  const todo = await getTodoById(id)
+
+  if (!todo) return c.notFound()
+
+  const detail = await renderFile("views/detail.html", {
+    todo,
+  })
+
+  return c.html(detail)
+})
+
+app.post("/todos/:id", async (c) => {
+  const id = Number(c.req.param("id"))
+
+  const todo = await getTodoById(id)
+
+  if (!todo) return c.notFound()
+
+  const form = await c.req.formData()
+
+  await db
+    .update(todosTable)
+    .set({ title: form.get("title") })
+    .where(eq(todosTable.id, id))
+
+  return c.redirect(c.req.header("Referer"))
+})
+
+app.post("/todos/:id/rename", async (c) => {
+  const id = Number(c.req.param("id"))
+  const form = await c.req.formData()
+  const newTitle = form.get("title")
+  const newPriority = form.get("priority")
+
+  const todo = await getTodoById(id)
+  if (!todo) return c.notFound()
+
+  await db
+    .update(todosTable)
+    .set({ title: newTitle, priority: newPriority })
+    .where(eq(todosTable.id, id))
+
+  return c.redirect(`/todos/${id}`)
+})
+
+app.get("/todos/:id/toggle", async (c) => {
+  const id = Number(c.req.param("id"))
+
+  const todo = await getTodoById(id)
+
+  if (!todo) return c.notFound()
+
+  await db
+    .update(todosTable)
+    .set({ done: !todo.done })
+    .where(eq(todosTable.id, id))
+
+  return c.redirect(c.req.header("Referer"))
+})
+
+app.get("/todos/:id/remove", async (c) => {
+  const id = Number(c.req.param("id"))
+
+  const todo = await getTodoById(id)
+
+  if (!todo) return c.notFound()
+
+  await db.delete(todosTable).where(eq(todosTable.id, id))
+
+  return c.redirect("/")
+})
+
+serve(app, (info) => {
+  console.log(
+    `App started on http://localhost:${info.port}`
+  )
+})
+
+const getTodoById = async (id) => {
+  const todo = await db
+    .select()
+    .from(todosTable)
+    .where(eq(todosTable.id, id))
+    .get()
+
+  return todo
+}
---
+++ ukol_5/src/schema.js
@@ -0,0 +1,16 @@
+import {
+  sqliteTable,
+  int,
+  text,
+} from "drizzle-orm/sqlite-core"
+
+export const todosTable = sqliteTable("todos", {
+  id: int().primaryKey({ autoIncrement: true }),
+  title: text().notNull(),
+  done: int({ mode: "boolean" }).notNull(),
+  priority: text("priority", {
+    enum: ["spinkej", "cajk", "hoří ti koudel"],
+  })
+    .notNull()
+    .default("cajk"),
+})
